069f0f0f4faf8b1f722adf46ce90f8e691befb58
Emmanuel Amador <emmanuel.amador@endava.com>
2026-03-07 04:22:07 +0000

    Modify the method of how  mcp is executed

--- mcp_servers/big_query/app/main.py
+++ mcp_servers/big_query/app/main.py
@@ -1,4 +1,5 @@
 from .mcp_server import mcp
 
-# Create ASGI application, check: https://github.com/modelcontextprotocol/python-sdk/tree/main
-app = mcp.streamable_http_app()
+# Create ASGI application, check: https://github.com/modelcontextprotocol/python-sdk/tree/main and
+# https://github.com/modelcontextprotocol/python-sdk/tree/main?tab=readme-ov-file#quickstart
+app = mcp.run(transport="streamable-http")
