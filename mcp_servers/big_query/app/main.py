8200d6700ff50cfc72896a73a7d7eddf5897a373
Emmanuel Amador <emmanuel.amador@endava.com>
2026-03-07 04:12:55 +0000

    Run the MCP server with the right method

--- mcp_servers/big_query/app/main.py
+++ mcp_servers/big_query/app/main.py
@@ -1,4 +1,4 @@
 from .mcp_server import mcp
 
 # Create ASGI application, check: https://github.com/modelcontextprotocol/python-sdk/tree/main
-app = mcp.run(transport="streamable-http")
+app = mcp.streamable_http_app()
