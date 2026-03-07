02669744852fc9f6f9b042d65d21bbadec1a2e4c
Emmanuel Amador <emmanuel.amador@endava.com>
2026-03-07 05:07:35 +0000

    Run the main.py module directly, which behind creates the uvicorn server

--- mcp_servers/big_query/app/main.py
+++ mcp_servers/big_query/app/main.py
@@ -2,4 +2,4 @@ from .mcp_server import mcp
 
 # Create ASGI application, check: https://github.com/modelcontextprotocol/python-sdk/tree/main and
 # https://github.com/modelcontextprotocol/python-sdk/tree/main?tab=readme-ov-file#quickstart
-app = mcp.sse_app()
+mcp.run(transport="streamable-http")
