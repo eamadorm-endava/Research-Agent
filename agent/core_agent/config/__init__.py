50d13b152f8f4179b7aae5215ed3396de47e3669
Emmanuel Amador <emmanuel.amador@endava.com>
2026-04-11 03:24:55 +0000

    Add constructor for the config module, including all the necessary config classes

---
+++ agent/core_agent/config/__init__.py
@@ -0,0 +1,19 @@
+from .agent_settings import AgentConfig, GCPConfig, GoogleAuthConfig
+from .mcp_settings import (
+    BaseMCPConfig,
+    BigQueryMCPConfig,
+    CalendarMCPConfig,
+    DriveMCPConfig,
+    GCSMCPConfig,
+)
+
+__all__ = [
+    "AgentConfig",
+    "GCPConfig",
+    "GoogleAuthConfig",
+    "BaseMCPConfig",
+    "BigQueryMCPConfig",
+    "CalendarMCPConfig",
+    "DriveMCPConfig",
+    "GCSMCPConfig",
+]
