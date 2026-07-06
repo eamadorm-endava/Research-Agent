352c896628cf7f0565b525eca758e41d7e41b7d2
Emmanuel Amador <emmanuel.amador@endava.com>
2026-07-06 17:07:21 +0000

    Modify OutlookMCPConfig to get the right permissions and description

--- agent/core_agent/config/mcp_settings.py
+++ agent/core_agent/config/mcp_settings.py
@@ -234,15 +234,16 @@ class OutlookMCPConfig(BaseMCPConfig):
     URL: str = "http://localhost:8087"
     OAUTH_SCOPES: Union[dict[str, str], list[str]] = [
         "User.Read",
-        "Files.Read.All",
-        "Sites.Read.All",
+        "Mail.Read",
+        "email",
         "offline_access",
+        "openid",
     ]
     GEMINI_AUTH_ID: Annotated[
         Optional[str],
         Field(
             default="mock-ms-auth-id",
-            description="Auth Resource ID for Microsoft SharePoint.",
+            description="Auth Resource ID for Microsoft Outlook.",
             validation_alias=AliasChoices(
                 "OUTLOOK_AUTH_ID",
                 "GEMINI_MICROSOFT_AUTH_ID",
